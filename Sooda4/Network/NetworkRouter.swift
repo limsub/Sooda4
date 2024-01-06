1b92414da889130aafc837949ef1d108a0575b9b
limsss9076@naver.com <limsss9076@naver.com>
2024-01-06 17:38:57 +0900

    [Feat] NetworkRouter Router pattern 구현

---
+++ Sooda4/Network/NetworkRouter.swift
@@ -0,0 +1,88 @@
+//
+//  NetworkRouter.swift
+//  Sooda4
+//
+//  Created by 임승섭 on 1/6/24.
+//
+
+import Foundation
+import Alamofire
+
+enum NetworkRouter: URLRequestConvertible {
+    
+    /* === 1. case === */
+    case checkValidEmail(_ sender: String?)// DTO
+    
+    
+    /* === 2. path === */
+    var path: String {
+        switch self {
+        case .checkValidEmail(let _):
+            return "/v1/users/validation/email"
+        }
+    }
+    
+    
+    /* === 3. header === */
+    var header: HTTPHeaders {
+        switch self {
+        case .checkValidEmail(let _):
+            return [
+                "Content-Type": "application/json",
+                "SesacKey": APIKey.key
+            ]
+        }
+    }
+    
+    
+    /* === 4. method === */
+    var method: HTTPMethod {
+        switch self {
+        case .checkValidEmail(let _):
+            return .post
+        }
+    }
+    
+    
+    /* === 5. parameter === */
+    var parameter: [String: Any] {
+        switch self {
+        case .checkValidEmail(let _):
+            return [
+                "email": "hi"
+            ]
+            
+        default:
+            return [:]
+        }
+    }
+    
+    
+    /* === 6. query === */
+    var query: [String: String] {
+        switch self {
+        default:
+            return [:]
+        }
+    }
+    
+    
+    /* === 7. asURLRequest === */
+    func asURLRequest() throws -> URLRequest {
+        let url = URL(string: APIKey.baseURL + path)!
+        
+        var request = URLRequest(url: url)
+        request.headers = header
+        request.method = method
+        
+        // paramter
+        if method == .post || method == .put {
+            let jsonData = try? JSONSerialization.data(withJSONObject: parameter)
+            request.httpBody = jsonData
+            
+            return request
+        }
+        
+        return request
+    }
+}
