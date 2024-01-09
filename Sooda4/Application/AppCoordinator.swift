a9b61e7496041050237a7f6a9e7e461aebaeb781
limsss9076@naver.com <limsss9076@naver.com>
2024-01-09 09:20:59 +0900

    [Fix] 부모 코디에서 coordinatorDidFinish 메서드 호출되었을 때, 분기처리 구상. App코디 적용 완료

--- Sooda4/Application/AppCoordinator.swift
+++ Sooda4/Application/AppCoordinator.swift
@@ -94,40 +94,38 @@ extension AppCoordinator: CoordinatorFinishDelegate {
         childCoordinator: Coordinator,
         nextFlow: ChildCoordinatorTypeProtocol?
     ) {
-        print(#function, Swift.type(of: self))
-        
-        print("매개변수로 받은 nextFlow : ", nextFlow)
-        
-        
+
         childCoordinators = childCoordinators.filter { $0.type != childCoordinator.type }
         navigationController.viewControllers.removeAll()
         // present된 애들도 없애줘야 하지 않을까..? navigationController.dismiss??
+        // -> 일단 이 navController에서 present된 애들은 없어. LoginScene에서는 dismiss 해주고 있다.
         
+        
+        /* App코디에게 연락이 온다.
+         1. Child
+         2. Child의 Child
+         */
+        
+        // 1 .Child
         if let nextFlow = nextFlow as? ChildCoordinatorType {
             switch nextFlow {
             case .splash:
                 print("여기로 가자고 하는 애는 없어야 해...")
+                
             case .loginScene:
-                // TODO: - show LoginFlow
                 self.showLoginFlow()
-                break;
+                
             case .tabBarScene:
-                // TODO: - show TabBarFlow
-                self.showTabBarFlow(workSpaceId: 100)
-                break;
+                print("이것도 실행되면 안된다. 탭바 코디 중 정확히 어디로 갈지 전달받아야 한다")
+                
             case .homeEmptyScene:
-                // TODO: - show HomeEmptyFlow
                 self.showHomeEmptyFlow()
                 
-                
-                break;
             }
         }
         
-        
-        // 통일 좀 시켜야겠다 이거
-        
-        
+        // 2. Child의 Child
+        // - 이렇게 갈 수 있는건 일단 TabBar코디 쪽밖에 없음
         if let nextFlow = nextFlow as? TabBarCoordinator.ChildCoordinatorType {
             switch nextFlow {
             case .homeDefaultScene(let workSpaceId):
@@ -140,17 +138,15 @@ extension AppCoordinator: CoordinatorFinishDelegate {
                 break;
             }
         }
-        
-        
     }
 }
 
 // MARK: - Child Coordinator 타입
 extension AppCoordinator {
     enum ChildCoordinatorType: ChildCoordinatorTypeProtocol {
-        case splash, loginScene, tabBarScene
-        
+        case splash
+        case loginScene
+        case tabBarScene
         case homeEmptyScene
-        
     }
 }
