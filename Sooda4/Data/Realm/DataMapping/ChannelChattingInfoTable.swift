2cdd498772c24573847cac45eb1c51c12cd7b52c
limsss9076@naver.com <limsss9076@naver.com>
2024-01-24 14:30:53 +0900

    [Fix] [Realm] 테이블 참조 타입 수정. LinkingObjects 삭제 -> 역방향 x. 단순 테이블 타입으로 수정

--- Sooda4/Data/Realm/DataMapping/ChannelChattingInfoTable.swift
+++ Sooda4/Data/Realm/DataMapping/ChannelChattingInfoTable.swift
@@ -12,10 +12,15 @@ class ChannelChattingInfoTable: Object {
     
     @Persisted(primaryKey: true) var chat_id: Int
     
-    @Persisted(originProperty: "channel_id") var channelInfo: LinkingObjects<ChannelInfoTable>
-    @Persisted(originProperty: "user_id") var userInfo: LinkingObjects<UserInfoTable>
+//    @Persisted(originProperty: "channel_id") var channelInfo: LinkingObjects<ChannelInfoTable>
+    
+    @Persisted var channelInfo: ChannelInfoTable
+    @Persisted var userInfo: UserInfoTable
     
     @Persisted var content: String
     @Persisted var createdAt: Date
     @Persisted var files: List<String>
 }
+
+
+
