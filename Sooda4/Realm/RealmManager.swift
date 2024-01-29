0974156b47dffe3bc8ae7261c2f5715d9e9e9987
limsss9076@naver.com <limsss9076@naver.com>
2024-01-29 18:43:28 +0900

    q

---
+++ Sooda4/Realm/RealmManager.swift
@@ -0,0 +1,19 @@
+//
+//  RealmManager.swift
+//  Sooda4
+//
+//  Created by 임승섭 on 1/29/24.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager {
+    
+    static let shared = RealmManager()
+    
+    private init() { }
+    
+    func configure
+    
+}
